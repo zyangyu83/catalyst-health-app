6e82a480ba12cc67e44dca27311be1108f53f232
Cassie <cassie@CassiedeMacBook-Air.local>
2026-04-19 16:10:33 +0800

    fix: 解决404问题

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import "./styles/index.css";
+import { HashRouter } from "react-router-dom"; // 1. 导入 HashRouter
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <HashRouter> {/* 2. 把 App 包在 HashRouter 里 */}
+    <App />
+  </HashRouter>
+);
